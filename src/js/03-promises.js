3e7c0136335b5ed7acbc175a49a5e8453fe561be
Ichim99 <140713412+Ichim99@users.noreply.github.com>
2024-01-25 17:35:29 +0200

    first commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+Notify.init({
+  fontSize: "1rem",
+  width: "450px",
+  cssAnimationStyle: "from-bottom",
+  closeButton: false,
+  useIcon: false,
+  pauseOnHover: true,
+});
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
